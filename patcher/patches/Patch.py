6c7ae786f0b55f7af5d20c16e9d5246f508ace73
Tommy <76645845+Tom-stack3@users.noreply.github.com>
2024-12-27 15:07:44 +0200

    Adjust Patcher and Extractor code

---
+++ patcher/patches/Patch.py
@@ -0,0 +1,12 @@
+class Patch:
+    def __init__(self, extracted_path):
+        self.extracted_path = extracted_path
+        self.print_message = "Applying patch..."
+        self.class_data = None
+        self.class_path = None
+
+    def class_filter(self, class_data: str) -> bool:
+        raise NotImplementedError
+
+    def class_modifier(self, class_data: str) -> str:
+        raise NotImplementedError
