24b6b590569f41dc0b7d58a40a8a262f77d79f76
Tommy <76645845+Tom-stack3@users.noreply.github.com>
2025-06-20 14:17:46 +0300

    Use objection

--- utils.sh
+++ utils.sh
@@ -2,6 +2,8 @@
 source secrets
 ADB="/mnt/c/Program Files/Genymobile/Genymotion/tools/adb.exe"
 FRIDA="/mnt/c/Users/$USERNAME/AppData/Local/Programs/Python/Python312/Scripts/frida.exe"
+OBJECTION="/mnt/c/Users/$USERNAME/AppData/Local/Programs/Python/Python312/Scripts/objection.exe"
+PACKAGE_NAME="com.tranzmate"
 
 create_rev_proxy_first_time() {
     mkdir -p /tmp/burp && cd /tmp/burp
@@ -46,27 +48,32 @@ apply_patches() {
 
 install_patched_apk() {
     echo "[+] Uninstalling app and installing patched APK..."
-    "$ADB" uninstall com.tranzmate && echo "Uninstalled original app successfully" || echo "[-] Original app not found!"
+    "$ADB" uninstall "$PACKAGE_NAME" && echo "Uninstalled original app successfully" || echo "[-] Original app not found!"
     echo "[+] Installing patched APK..."
     "$ADB" install -r moovitpatched_5.145.apk && echo "[+] Patched APK installed successfully" || echo "[-] Failed to install patched APK"
 }
 
 install_original_apk() {
     echo "[+] Uninstalling app and installing original APK..."
-    "$ADB" uninstall com.tranzmate && echo "Uninstalled original app successfully" || echo "[-] Original app not found!"
+    "$ADB" uninstall "$PACKAGE_NAME" && echo "Uninstalled original app successfully" || echo "[-] Original app not found!"
     echo "[+] Installing original APK..."
     "$ADB" install -r moovit_5.145.apk && echo "[+] Patched APK installed successfully" || echo "[-] Failed to install patched APK"
 }
 
 run_app_with_frida() {
     echo "[+] Running app with Frida..."
-    "$FRIDA" -U -f com.tranzmate -l frida.js
+    "$FRIDA" -U -f "$PACKAGE_NAME" -l frida.js
 }
 
 run_frida_server() {
     "$ADB" shell ps | grep frida-server && echo "Frida server is already running!" && return
     echo "[+] Running Frida on the device..."
     echo "/data/local/tmp/frida-server &" | "$ADB" shell su
+}
+
+run_app_with_objection() {
+    echo "[+] Running app with Objection..."
+    "$OBJECTION" --gadget "$PACKAGE_NAME" explore
 }
 
 if [ "$1" == "1" ]; then
@@ -83,6 +90,8 @@ elif [ "$1" == "6" ]; then
     run_frida_server
 elif [ "$1" == "7" ]; then
     run_app_with_frida
+elif [ "$1" == "8" ]; then
+    run_app_with_objection
 else
     echo "Usage: $0 <option>"
     echo "1 - Create reverse proxy with Burp Suite for the first time"
@@ -92,4 +101,5 @@ else
     echo "5 - Install original APK"
     echo "6 - Run Frida server on the device"
     echo "7 - Run app with Frida script"
+    echo "8 - Run app with Objection"
 fi
